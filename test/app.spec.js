dacef9b5afc6e63f453c25e4a20571fcc79758e7
ironpup <christoph.ding@gmail.com>
2017-02-16 10:49:09 -0500

    Add test that uses a stub to modify the behavior of an existing function.  Clean up code with comments.

--- test/app.spec.js
+++ test/app.spec.js
@@ -9,19 +9,21 @@ describe('A very simple test', () => {
     it('can run tests', ()=> {
       let testDataFunctionReturnValue = data.testDataFunction();
       expect(testDataFunctionReturnValue).to.eql('final value');
-    })
+    });
 
     it('can use spies to get see how existing functions are used', () => {
-      // spies
+      // set up test
       let spyDataFunction = sinon.spy(data, 'testDataFunction');
 
       data.testDataFunction();
       data.testDataFunction();
 
+      // test
       expect(spyDataFunction.callCount).to.eql(2);
-    })
+    });
 
-    it('can use spies to see how helper functions are called indirectly', () => {      
+    it('can use spies to see how helper functions are called indirectly', () => {
+      // set up test
       var aThing = {
         main() {
           this.helper();
@@ -30,18 +32,33 @@ describe('A very simple test', () => {
           return 'some value';
         }  
       }
-      // spies
       sinon.spy(aThing, 'helper');      
       aThing.main();
 
+      // test
       expect(aThing.helper.callCount).to.eql(1);
-    })
+    });
 
     it('can use spies on helper functions in other modules', () => {
-      // spies
+      // set up test
       sinon.spy(data, 'helperFunction');
       data.testDataFunction();
+
+      // test
       expect(data.helperFunction.callCount).to.eql(1);
-    })
+    });
+
+    it('can use stubs to affect functionality', () => {
+      // set up test
+      data.helperFunction.restore();      
+      const replacementFunction = () => {
+        return 'value altered by a sinon stub!';
+      }
+      sinon.stub(data, 'helperFunction', replacementFunction);
+
+      // test
+      let testDataFunctionReturnValue = data.testDataFunction();
+      expect(testDataFunctionReturnValue).to.eql('value altered by a sinon stub!');
+    });
 
 })
